27e07ba9928d6eef780601eb9785c41fe05f2315
Kirill Terekhov <kirill.terehov@gmail.com>
2023-02-08 13:15:13 +0300

    two ellipses in test

--- test_slice_ellipse.cpp
+++ test_slice_ellipse.cpp
@@ -3,49 +3,79 @@
 using namespace INMOST;
 
 
-//ellipse (x - ec)^T eA (x - ec) = 1
-double eA[6] =
-{
-	4.0, 0.1, 0.0,
-	     6.0, 0.1,
-		      24.0
-};
-double ec[3] = { 0.5,0.5,0.5 };
-
-class SliceEllipse : public Slice
+//ellipse (x - c)^T A (x - c) = 1
+class Ellipse
 {
 	double A[6], c[3];
-	Storage::real func(Storage::real x, Storage::real y, Storage::real z) const
+public:
+	Ellipse()
 	{
-		Storage::real xc[3], Axc[3];
+		std::fill(A, A + 6, 0.0);
+		std::fill(c, c + 3, 0.0);
+	}
+	Ellipse(double Axx, double Axy, double Axz, double Ayy, double Ayz, double Azz, double cx, double cy, double cz)
+	{
+		A[0] = Axx;
+		A[1] = Axy;
+		A[2] = Axz;
+		A[3] = Ayy;
+		A[4] = Ayz;
+		A[5] = Azz;
+		c[0] = cx;
+		c[1] = cy;
+		c[2] = cz;
+	}
+	Ellipse(double Ain[6], double cin[3])
+	{
+		std::copy(Ain, Ain + 6, A);
+		std::copy(cin, cin + 3, c);
+	}
+	Ellipse(const Ellipse& b)
+	{
+		std::copy(b.A, b.A + 6, A);
+		std::copy(b.c, b.c + 3, c);
+	}
+	Ellipse& operator =(Ellipse const& b)
+	{
+		if (this != &b)
+		{
+			std::copy(b.A, b.A + 6, A);
+			std::copy(b.c, b.c + 3, c);
+		}
+		return *this;
+	}
+	double func(double x, double y, double z) const
+	{
+		double xc[3], Axc[3];
 		xc[0] = x - c[0];
 		xc[1] = y - c[1];
 		xc[2] = z - c[2];
 		Axc[0] = A[0] * xc[0] + A[1] * xc[1] + A[2] * xc[2];
 		Axc[1] = A[1] * xc[0] + A[3] * xc[1] + A[4] * xc[2];
 		Axc[2] = A[2] * xc[0] + A[4] * xc[1] + A[5] * xc[2];
-		
 		return 1.0 - (xc[0] * Axc[0] + xc[1] * Axc[1] + xc[2] * Axc[2]);
 	}
+};
+
+class SliceEllipse : public Slice
+{
+	std::vector<Ellipse> ellipses;
+	double func(double x, double y, double z) const
+	{
+		double fmax = -1.0e+20;
+		for (size_t k = 0; k < ellipses.size(); ++k)
+			fmax = std::max(fmax, ellipses[k].func(x, y, z));
+		return fmax;
+	}
 public:
-	SliceEllipse(double Ain[6], double cin[3]) :Slice() 
-	{
-		std::copy(Ain, Ain + 6, A);
-		std::copy(cin, cin + 3, c);
-	}
-	SliceEllipse(const SliceEllipse &b) :Slice(b) 
-	{
-		std::copy(b.A, b.A + 6, A);
-		std::copy(b.c, b.c + 3, c);
-	}
+	SliceEllipse() :Slice() {}
+	SliceEllipse(const SliceEllipse &b) :Slice(b), ellipses(b.ellipses) {}
+	void AddEllipse(const Ellipse& E) { ellipses.push_back(E); }
 	SliceEllipse & operator =(SliceEllipse const & b) 
 	{ 
 		Slice::operator =(b);
 		if (this != &b)
-		{
-			std::copy(b.A, b.A + 6, A);
-			std::copy(b.c, b.c + 3, c);
-		}
+			ellipses = b.ellipses;
 		return *this;
 	}
 	Storage::real LevelFunction(Storage::real p[3]) const {return func(p[0],p[1],p[2]);}
@@ -75,8 +105,12 @@ int main(int argc, char ** argv)
 	
 	std::cout << "Cells: " << m.NumberOfCells() << std::endl;
 	std::cout << "Faces: " << m.NumberOfFaces() << std::endl;
+
 	
-	SliceEllipse(eA, ec).SliceMesh(m, true);
+	SliceEllipse slice;
+	slice.AddEllipse(Ellipse(4.0, 0.1, 0.0, 6.0, 0.1, 24.0, 0.5, 0.5, 0.5));
+	slice.AddEllipse(Ellipse(6.0, 1.0, -10.0, 4.0, 1.0, 32.0, 0.7, 0.5, 0.8));
+	slice.SliceMesh(m, true);
 	
 	std::cout << "Cells: " << m.NumberOfCells() << std::endl;
 	std::cout << "Faces: " << m.NumberOfFaces() << std::endl;
